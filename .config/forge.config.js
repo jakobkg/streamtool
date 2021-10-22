9c3423c983468960446375366ec0e8ad6b5c86e4
Jakob Grønhaug <jakob@gronha.ug>
2021-10-22 21:46:35 +0200

    Change ReactDOM render context from TS to TSX

--- .config/forge.config.js
+++ .config/forge.config.js
@@ -42,7 +42,7 @@ module.exports = {
           entryPoints: [
             {
               html: "./src/renderer/index.html",
-              js: "./src/renderer.ts",
+              js: "./src/renderer.tsx",
               name: "main_window",
               preload: {
                 js: "./src/preload.ts",
