2ccf2b4206d74f5e309ee9a1ded37ea8cf47e5a2
Jakob Grønhaug <jakob@gronha.ug>
2021-10-22 21:10:14 +0200

    add non-working hook

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,12 +1,14 @@
 import Sidebar from "./Components/Sidebar";
 import MainView from "./Components/MainView";
+import { useState } from "react";
 
 export default function App() {
+  const [clicked, setClicked] = useState(false);
 
   return (
     <div className="flex flex-row bg-gray-800 w-screen h-screen">
-      <MainView text={'aeiou'}/>
-      <Sidebar />
+      <MainView text={clicked ? 'a' : 'b'}/>
+      <Sidebar clickState={clicked} clickCallback={setClicked} />
     </div>
   )
 }
